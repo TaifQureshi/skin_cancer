545100e848fa2bb2e4f0c239f640558b8dae7bdf
TaifQureshi <54901175+TaifQureshi@users.noreply.github.com>
2020-04-09 15:32:28 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+from PIL import Image
+import os
+np.random.seed(123)
+import tensorflow
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+
+#path
+base_dir = os.path.join('..', 'skin-cancer-mnist-ham10000')
+imageid_path_dict = {os.path.splitext(os.path.basename(x))[0]:x for x in glob(os.path.join(base_dir, '*', '*.jpg'))}
+
+lesion_type_dict = {
+    'nv': 'Melanocytic nevi',
+    'mel': 'Melanoma',
+    'bkl': 'Benign keratosis-like lesions ',
+    'bcc': 'Basal cell carcinoma',
+    'akiec': 'Actinic keratoses',
+    'vasc': 'Vascular lesions',
+    'df': 'Dermatofibroma'
+}
+
+#EDA
+skin_df = pd.read_csv(os.path.join(base_dir, 'HAM10000_metadata.csv'))
+skin_df['path'] = skin_df['image_id'].map(imageid_path_dict.get)
+skin_df['cell_type'] = skin_df['dx'].map(lesion_type_dict.get) 
+skin_df['cell_type_idx'] = pd.Categorical(skin_df['cell_type']).codes
+skin_df['age'].fillna((skin_df['age'].mean()), inplace=True)
+skin_df['image'] = skin_df['path'].map(lambda x: np.asarray(Image.open(x).resize((100,75))))
+features=skin_df.drop(columns=['cell_type_idx'],axis=1)
+target=skin_df['cell_type_idx']
+
+x_train_o, x_test_o, y_train_o, y_test_o = train_test_split(features, target, test_size=0.20,random_state=1234)
+x_train = np.asarray(x_train_o['image'].tolist())
+x_test = np.asarray(x_test_o['image'].tolist())
+
+x_train_mean = np.mean(x_train)
+x_train_std = np.std(x_train)
+
+x_test_mean = np.mean(x_test)
+x_test_std = np.std(x_test)
+
+x_train = (x_train - x_train_mean)/x_train_std
+x_test = (x_test - x_test_mean)/x_test_std
+y_train = tensorflow.keras.utils.to_categorical(y_train_o, num_classes = 7)
+y_test = tensorflow.keras.utils.to_categorical(y_test_o, num_classes = 7)
+
+x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size = 0.1, random_state = 2)
+x_train = x_train.reshape(x_train.shape[0], *(75, 100, 3))
+x_test = x_test.reshape(x_test.shape[0], *(75, 100, 3))
+x_validate = x_validate.reshape(x_validate.shape[0], *(75, 100, 3))
+
+datagen = ImageDataGenerator(
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1 ) # randomly shift images vertically (fraction of total height)
+
+
+input_shape = (75, 100, 3)
+num_classes = 7
+
+model = tensorflow.keras.Sequential()
+
+#input
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',padding = 'Same',input_shape=input_shape))
+
+#hidden1
+model.add(Conv2D(32,kernel_size=(3, 3), activation='relu',padding = 'Same',))
+model.add(MaxPool2D(pool_size = (2, 2)))
+model.add(Dropout(0.25))
+
+#hidden2
+model.add(Conv2D(64, (3, 3), activation='relu',padding = 'Same'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.3))
+
+#hidden3
+model.add(Conv2D(64, (3, 3), activation='relu',padding = 'Same'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.3))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(num_classes, activation='softmax'))
+
+optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+datagen.fit(x_train)
+epochs = 50 
+batch_size = 10
+history = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
+                              epochs = epochs, validation_data = (x_validate,y_validate),
+                              verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size
+                              , callbacks=[learning_rate_reduction])
+
+loss, accuracy = model.evaluate(x_test, y_test, verbose=1)
+loss_v, accuracy_v = model.evaluate(x_validate, y_validate, verbose=1)
+print("Validation: accuracy = %f  ;  loss_v = %f" % (accuracy_v, loss_v))
+print("Test: accuracy = %f  ;  loss = %f" % (accuracy, loss))
+model.save("model.h5")

e301b382496ca539736513f76893308f52efe3bd
TaifQureshi <54901175+TaifQureshi@users.noreply.github.com>
2020-07-31 18:34:54 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,76 +1,57 @@
-import pandas as pd
 import numpy as np
-from glob import glob
-from PIL import Image
+import cv2
+import pandas as pd
 import os
+from glob import glob
 np.random.seed(123)
-import tensorflow
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import ReduceLROnPlateau
-
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+from tensorflow.keras import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-#path
-base_dir = os.path.join('..', 'skin-cancer-mnist-ham10000')
+base_dir = os.path.join('..', '/content/train')
 imageid_path_dict = {os.path.splitext(os.path.basename(x))[0]:x for x in glob(os.path.join(base_dir, '*', '*.jpg'))}
 
 lesion_type_dict = {
-    'nv': 'Melanocytic nevi',
-    'mel': 'Melanoma',
-    'bkl': 'Benign keratosis-like lesions ',
-    'bcc': 'Basal cell carcinoma',
-    'akiec': 'Actinic keratoses',
-    'vasc': 'Vascular lesions',
-    'df': 'Dermatofibroma'
+    'nv': 0,
+    'mel': 1,
+    'bkl': 2,
+    'bcc': 3,
+    'akiec': 4,
+    'vasc': 5,
+    'df': 6
 }
 
-#EDA
 skin_df = pd.read_csv(os.path.join(base_dir, 'HAM10000_metadata.csv'))
 skin_df['path'] = skin_df['image_id'].map(imageid_path_dict.get)
-skin_df['cell_type'] = skin_df['dx'].map(lesion_type_dict.get) 
-skin_df['cell_type_idx'] = pd.Categorical(skin_df['cell_type']).codes
-skin_df['age'].fillna((skin_df['age'].mean()), inplace=True)
-skin_df['image'] = skin_df['path'].map(lambda x: np.asarray(Image.open(x).resize((100,75))))
-features=skin_df.drop(columns=['cell_type_idx'],axis=1)
-target=skin_df['cell_type_idx']
+skin_df['cell_type'] = skin_df['dx'].map(lesion_type_dict.get)
+skin_df['image'] = skin_df['path'].map(lambda x: cv2.resize(cv2.imread(x, cv2.IMREAD_GRAYSCALE),(75,75)))
 
-x_train_o, x_test_o, y_train_o, y_test_o = train_test_split(features, target, test_size=0.20,random_state=1234)
-x_train = np.asarray(x_train_o['image'].tolist())
-x_test = np.asarray(x_test_o['image'].tolist())
+data = pd.DataFrame(columns=['image','lable'])
+data['image'] = skin_df['image']
+data['lable'] = skin_df['cell_type']
+data.dropna(inplace=True)
 
-x_train_mean = np.mean(x_train)
-x_train_std = np.std(x_train)
+x_train = np.asarray(data['image'].tolist()).reshape(-1,75,75,1)
+y_train = data['lable']
+y_train = pd.get_dummies(y_train)
+x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size = 0.1, random_state = 125)
 
-x_test_mean = np.mean(x_test)
-x_test_std = np.std(x_test)
-
-x_train = (x_train - x_train_mean)/x_train_std
-x_test = (x_test - x_test_mean)/x_test_std
-y_train = tensorflow.keras.utils.to_categorical(y_train_o, num_classes = 7)
-y_test = tensorflow.keras.utils.to_categorical(y_test_o, num_classes = 7)
-
-x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size = 0.1, random_state = 2)
-x_train = x_train.reshape(x_train.shape[0], *(75, 100, 3))
-x_test = x_test.reshape(x_test.shape[0], *(75, 100, 3))
-x_validate = x_validate.reshape(x_validate.shape[0], *(75, 100, 3))
+input_shape = (75, 75,1)
+num_classes = 7
 
 datagen = ImageDataGenerator(
         rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
         zoom_range = 0.1, # Randomly zoom image 
         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1 ) # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.1 )
 
 
-input_shape = (75, 100, 3)
-num_classes = 7
-
-model = tensorflow.keras.Sequential()
+model = Sequential()
 
 #input
 model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',padding = 'Same',input_shape=input_shape))
@@ -102,16 +83,15 @@ learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
                                             verbose=1, 
                                             factor=0.5, 
                                             min_lr=0.00001)
+
 datagen.fit(x_train)
 epochs = 50 
 batch_size = 10
+
 history = model.fit_generator(datagen.flow(x_train,y_train, batch_size=batch_size),
                               epochs = epochs, validation_data = (x_validate,y_validate),
                               verbose = 1, steps_per_epoch=x_train.shape[0] // batch_size
                               , callbacks=[learning_rate_reduction])
 
-loss, accuracy = model.evaluate(x_test, y_test, verbose=1)
-loss_v, accuracy_v = model.evaluate(x_validate, y_validate, verbose=1)
-print("Validation: accuracy = %f  ;  loss_v = %f" % (accuracy_v, loss_v))
-print("Test: accuracy = %f  ;  loss = %f" % (accuracy, loss))
-model.save("model.h5")
+
+model.save("mode_skincancer.h5")
